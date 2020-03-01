98087d0c263bffa80bfaa87c9a4f520aa1e90d90
Max <max@iMac-de-Max.local>
2020-03-01 20:54:14 -0300

    Ajuste condicional pipe de ordenar por nome

--- src/app/order.pipe.ts
+++ src/app/order.pipe.ts
@@ -3,7 +3,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'ordenarNome'})
 export class OrdenarNome implements PipeTransform {
   transform(array: any[]) {
-    if (array !== undefined)
+    if (array)
     return array.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
     return array;
   }
